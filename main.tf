d8c7af49fd8bc9e2ad135881327a344933a59d3c
Justin Biard <icodealot@gmail.com>
2025-01-04 10:33:28 -0600

    wip

--- main.tf
+++ main.tf
@@ -11,17 +11,19 @@ provider "docker" {
   host = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "nginx" {
+resource "docker_image" "frontend_image" {
   name         = "nginx"
   keep_locally = false
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+resource "docker_container" "frontend_container" {
+  image = docker_image.frontend_image.image_id
+  name  = "cyc_frontend_2025"
 
   ports {
     internal = 80
     external = 8000
   }
+
+  network_mode = "bridge"
 }
