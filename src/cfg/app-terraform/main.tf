750b018af9af90d18da1837ea3be86ccd384d20b
Justin Biard <icodealot@gmail.com>
2025-09-20 13:52:16 -0500

    demo for locals

--- src/cfg/app-terraform/main.tf
+++ src/cfg/app-terraform/main.tf
@@ -2,13 +2,17 @@ data "docker_image" "backend_image" {
   name = "cyc-backend:latest"
 }
 
+locals {
+  service_port = 8080
+}
+
 resource "docker_container" "backend_container" {
   image = data.docker_image.backend_image.id
   name  = "cyc_backend_2025-dev"
 
   ports {
     internal = 8000
-    external = 8080
+    external = local.service_port
   }
 
   network_mode = "bridge"
