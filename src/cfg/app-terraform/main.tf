995f08f73197f162698e2b2d7e9e8fe9071c1414
Justin Biard <icodealot@gmail.com>
2025-09-20 14:19:56 -0500

    add variable demo

--- src/cfg/app-terraform/main.tf
+++ src/cfg/app-terraform/main.tf
@@ -2,13 +2,26 @@ data "docker_image" "backend_image" {
   name = "cyc-backend:latest"
 }
 
+# For example:
+# terraform plan -var="stage=prod"
+
+variable "stage" {
+  type        = string
+  description = "Deployment stage (dev or prod)"
+  default     = "dev"
+}
+
+locals {
+  service_port = var.stage == "dev" ? 8080 : 9090
+}
+
 resource "docker_container" "backend_container" {
   image = data.docker_image.backend_image.id
   name  = "cyc_backend_2025-dev"
 
   ports {
     internal = 8000
-    external = 8080
+    external = local.service_port
   }
 
   network_mode = "bridge"
